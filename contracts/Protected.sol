be1142e2654ab87c8d49722f2583597dad9129fc
zach-is-my-name <zach.greco@gmail.com>
2020-08-23 19:28:42 -0400

    create productionVersion from testVersion

--- contracts/Protected.sol
+++ contracts/Protected.sol
@@ -29,10 +29,7 @@ contract Protected is EscrowRole, AionRole {
      // return true;
     }
 
-  /* this function's "onlyEscrowRole" exists only in this branch for ability to test (can't run Aion system locally).  In all other versions and branches t
-he onlyAionRole should be applied to restrict access to only the Aion contract which performs timed calls */
- 
-    function removeTokenProtection(address _address, uint256 _amount) public onlyEscrowRole returns (bool) {
+    function removeTokenProtection(address _address, uint256 _amount) public onlyAionRole returns (bool) {
       emit Caller(msg.sender);
       protectedTokens[_address] = protectedTokens[_address].sub(_amount);
       //return true;
