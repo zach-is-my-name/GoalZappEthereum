a53b8821d51df0b7c6ba83298710bb2bb3308a81
zach-is-my-name <zach.greco@gmail.com>
2021-03-24 16:50:56 -0400

    commented out truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,4 @@
+/*
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -100,3 +101,4 @@ module.exports = {
   },
   plugins: ["solidity-coverage"]
 }
+*/
